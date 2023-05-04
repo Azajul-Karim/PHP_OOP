03f33cffa3164169ed8ae6978fd8711aec3cbb14
Azajul-Karim <azajulkarim2021@gmail.com>
2023-05-04 13:16:29 +0600

    class and method added

---
+++ 01.class_&_method.php
@@ -0,0 +1,32 @@
+<?php
+/*
+access modifiers
+1.public
+2.private
+3.protected
+*/
+
+// -> = object operator
+
+class Person
+{
+  public $name = "Azajul Karim";
+  public $age;
+
+  //method
+  public function personName()
+  {
+    echo "Person Name Is : " . $this->name . "</br>";
+  }
+
+  //method
+  public function personAge($value)
+  {
+    echo "Person Age Is : " . $this->age = $value;
+  }
+}
+
+$personOne = new Person;
+$personOne->personName();
+
+$personOne->personAge(23);
